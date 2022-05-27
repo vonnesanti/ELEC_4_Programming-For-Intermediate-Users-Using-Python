7eafa9d55c3479a904c0e6043ed658dd9d619011
Yvonne Santiago <ybsantiago@iskolarngbayan.pup.edu.ph>
2022-05-27 15:30:37 +0800

    This Commit is for Exercise 3.4

---
+++ 3.4 Coding Exercise.py
@@ -0,0 +1,9 @@
+f = open("pythonessay.txt", "w")
+f.write("I like Python because it is very interesting.")
+f = open("pythonessay.txt", "a")
+f.write("\nI plan to learn data visualization.")
+f.write("\n I want to work in the field of data science.")
+f.write("\n I plan to make a better world for the future generation.")
+f = open("pythonessay.txt", "r")
+f.close()
+
